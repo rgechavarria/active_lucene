afd9f666e477d044bb1261ff35e06e6640b91500
Ricardo Echavarria <rgechavarria@gmail.com>
2011-11-23 11:59:14 -0700

    Choose lucene index path with a rails initializer

--- lib/active_lucene/index.rb
+++ lib/active_lucene/index.rb
@@ -1,9 +1,14 @@
 module ActiveLucene
   module Index
     PATH = "#{APP_ROOT}/db/lucene/#{APP_ENV}"
+    @@path = PATH
 
     def self.directory
-      FSDirectory.open java.io.File.new(PATH)
+      FSDirectory.open java.io.File.new(@@path)
+    end
+    
+    def self.path=(value)
+      @@path = value
     end
   end
 end
